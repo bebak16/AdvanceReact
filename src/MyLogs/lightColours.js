957830ce01efebc055d184788760f9046174b76b
bebak16 <bebak16@gmail.com>
2024-04-10 20:22:53 +0530

    updated colors

--- src/MyLogs/lightColours.js
+++ src/MyLogs/lightColours.js
@@ -1,33 +1,33 @@
 export const lightColors = [
-    "#F0F8FF", // Alice Blue
-    "#FAEBD7", // Antique White
-    "#00FFFF", // Aqua
-    "#7FFFD4", // Aquamarine
-    "#F0FFFF", // Azure
-    "#F5F5DC", // Beige
-    "#FFF5EE", // Sea Shell
-    "#FFE4C4", // Bisque
-    "#FFF8DC", // Cornsilk
-    "#FFFAF0", // Floral White
-    "#F0FFF0", // Honeydew
-    "#FFFFF0", // Ivory
-    "#F0E68C", // Khaki
-    "#E6E6FA", // Lavender
-    "#FFF0F5", // Lavender Blush
-    "#7CFC00", // Lawn Green
-    "#FFFACD", // Lemon Chiffon
-    "#FDF5E6", // Old Lace
-    "#FFE4B5", // Moccasin
-    "#FFE4E1", // Misty Rose
-    "#FFEBCD", // Blanched Almond
-    "#FFEFD5", // Papaya Whip
-    "#FAF0E6", // Linen
-    "#FFF5E0", // Seashell
-    "#FAFAD2", // Light Goldenrod Yellow
-    "#FAFAD2", // Light Yellow
-    "#FFE4E1", // Misty Rose
-    "#F5DEB3", // Wheat
-    "#FFDAB9", // Peach Puff
-    "#F5F5F5", // White Smoke
-  ];
-  
+  "#F0F8FF", // Alice Blue
+  "#FAEBD7", // Antique White
+  "#00FFFF", // Aqua
+  "#7FFFD4", // Aquamarine
+  "#F0FFFF", // Azure
+  "#F5F5DC", // Beige
+  "#FFF5EE", // Sea Shell
+  "#FFE4C4", // Bisque
+  "#FFF8DC", // Cornsilk
+  "#FFFAF0", // Floral White
+  "#F0FFF0", // Honeydew
+  "#FFFFF0", // Ivory
+  "#F0E68C", // Khaki
+  "#E6E6FA", // Lavender
+  "#7CFC00", // Lawn Green
+  "#FFFACD", // Lemon Chiffon
+  "#FDF5E6", // Old Lace
+  "#FFE4B5", // Moccasin
+  "#FFE4E1", // Misty Rose
+  "#FFEBCD", // Blanched Almond
+  "#FFEFD5", // Papaya Whip
+  "#FAF0E6", // Linen
+  "#FAFAD2", // Light Goldenrod Yellow
+  "#FAFAD2", // Light Yellow
+  "#FFE4E1", // Misty Rose
+  "#F5DEB3", // Wheat
+  "#FFDAB9", // Peach Puff
+  "#F5F5F5", // White Smoke
+  "#FFD700", // Gold (Added)
+  "#FF6347", // Tomato (Added)
+  "#20B2AA", // Light Sea Green (Added)
+];
